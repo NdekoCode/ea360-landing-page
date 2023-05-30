d26b6455b911773d27fc91e6686e1dc1442ea048
NdekoCode <arickbulakali@gmail.com>
2023-05-30 19:42:18 +0200

    feat: add mobile navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  isActive: boolean = false;
+  toggleActive() {
+    if (window.innerWidth < 992) {
+      this.isActive = !this.isActive;
+    }
+  }
 }
